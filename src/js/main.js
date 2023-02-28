bdd8ca1ad0e981f1913bb8b616f92432d298a234
Andrey <alvice01andru@gmail.com>
2023-02-28 22:29:53 +0300

    Рефакторинг модуля main

--- src/js/main.js
+++ src/js/main.js
@@ -1,58 +1,13 @@
-import {renderTodo, removeTodo, toggleTodoCheckedClass} from './todo-item.js';
-import {data} from './data.js';
+import view from './view.js';
+import data from './data.js';
 
+store.clearAll();
 
-
-const savedTodosArray = data.getTodos();
-
-if (savedTodosArray) {
-   savedTodosArray.forEach(i => {
-      renderTodo(i.text, i.id, i.checked);
-   })
-}
-
-
-
-const addBtn = document.querySelector('#add-btn');
-
-addBtn.addEventListener('click', onClickAddBtn);
-
-function onClickAddBtn(e) {
-   e.preventDefault();
-
-   const addField = document.querySelector('#add-field');
-
-   const todoText = addField.value;
-   addField.value = '';
-
-   data.addTodo(todoText)
-      .then((todoId) => {
-         renderTodo(todoText, todoId, false);
-      })
-}
-
-
-
-const todosWrapper = document.querySelector('#todos-wrapper');
-
-todosWrapper.addEventListener('click', onClickTodo);
-
-function onClickTodo(e) {
-   const element = e.target;
-
-   if (element.matches('.item-todo__delete')) {
-      const todoElement = element.closest('.item-todo');
-      const todoElementId = Number(todoElement.dataset.id);
-
-      data.deleteTodo(todoElementId);
-
-      removeTodo(todoElement);
-   } else if (element.matches('.item-todo__checkbox')) {
-      const todoElement = element.closest('.item-todo');
-      const todoElementId = Number(todoElement.dataset.id);
-
-      data.changeCheckedTodo(todoElementId);
-
-      toggleTodoCheckedClass(todoElement);
-   }
-}
+const addForm = document.querySelector('.todo-add-form');
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const addInput = document.querySelector('.todo-add-form__field');
+  const addInputValue = addInput.value;
+  data.addToDo(addInputValue, view.add.bind(view));
+  addInput.value = '';
+});
